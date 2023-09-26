49721dfe021ebf144111fc227e87b5eb8a88299f
Manuel Tellez <tellez.manuel94@gmail.com>
2023-09-26 13:54:42 -0500

    app modificado MT

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,65 @@
 <template>
-    <v-toolbar
-   color="#43A047">
-  
-    <v-img 
-    :max-width="70"
-    id="logonav"
-    src="../src/assets/logoBiotherapia.png"></v-img>
+  <v-app>
+  <v-toolbar color="#43A047">
+
+    <v-img :max-width="70" id="logonav" src="../src/assets/logoBiotherapia.png"></v-img>
     <v-toolbar-title>BioTherapia</v-toolbar-title>
-  
+
   </v-toolbar>
-    <v-card>
-      <v-layout>
-        <v-navigation-drawer color="#A5D6A7" class="mainmenu"
-          permanent
-          location="left"
-        >
-          <v-divider ></v-divider>
-          <v-list density="compact" nav>
-            <router-link class="rlnav" to="/"><v-list-item  prepend-icon="mdi mdi-home" title="INICIO" value="myfiles"></v-list-item></router-link>
-            <router-link class="rlnav"  to="/ventas"><v-list-item  prepend-icon="mdi mdi-server" title="INVENTARIO" value="shared"></v-list-item></router-link>
-            <router-link class="rlnav" to="/usuarios"><v-list-item  prepend-icon="mdi-account-multiple" title="USUARIOS" value="shared"></v-list-item></router-link>
-            <router-link class="rlnav" to="/crearusuarios"><v-list-item  prepend-icon="mdi-account-multiple" title="CREAR USUARIOS" value="shared"></v-list-item></router-link>
-            <router-link class="rlnav" to="/Login"><v-list-item  prepend-icon="mdi mdi-login" title="SIGN IN/SIGN UP" value="shared"></v-list-item></router-link>  
-          </v-list>
-        </v-navigation-drawer >
-        <v-main class="main">
-          <router-view></router-view>
-        </v-main> 
-      </v-layout>
-    </v-card>
-    <v-footer></v-footer>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer color="#A5D6A7" class="mainmenu" permanent location="left">
+        <v-divider></v-divider>
+        <v-list density="compact" nav>
+          <router-link class="rlnav" to="/"><v-list-item prepend-icon="mdi mdi-home" title="INICIO"
+              value="myfiles"></v-list-item></router-link>
+          <router-link class="rlnav" to="/ventas"><v-list-item prepend-icon="mdi mdi-server" title="INVENTARIO"
+              value="shared"></v-list-item></router-link>
+          <router-link class="rlnav" to="/usuarios"><v-list-item prepend-icon="mdi-account-multiple" title="USUARIOS"
+              value="shared"></v-list-item></router-link>
+          <router-link class="rlnav" to="/crearusuarios"><v-list-item prepend-icon="mdi-account-multiple"
+              title="CREAR USUARIOS" value="shared"></v-list-item></router-link>
+          <router-link class="rlnav" to="/Login"><v-list-item prepend-icon="mdi mdi-login" title="SIGN IN/SIGN UP"
+              value="shared"></v-list-item></router-link>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main class="main">
+        <router-view></router-view>
+      </v-main>
+    </v-layout>
+  </v-card>
+  <v-footer></v-footer>
+</v-app>
+</template>
   
-  </template>
-  
-  <style>
-  
-  .logo{
-    margin-left: 2%;
-  }
-  .mainmenu{
-  margin-top: 64px;
-  }
-  .main{
-    background-color: #C8E6C9;
-    height: 93vh;
-  }
-  #logonav{
-    margin-left: 1%;
-  }
-  .rlnav{
-    text-decoration: none;
-  }
-  </style>
-  <script>
-  
-  export default {
-    name: 'App',
-  
-    data: () => ({
-      //
-    }),
-  }
-  </script>
+<style>
+.logo {
+  margin-left: 2%;
+}
+
+
+
+.main {
+  background-color: #C8E6C9;
+  height: 93vh;
+}
+
+#logonav {
+  margin-left: 1%;
+}
+
+.rlnav {
+  text-decoration: none;
+}
+</style>
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
   
